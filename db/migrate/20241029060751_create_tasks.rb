4e76e5bae39ff0a21205c2a787114a05e0d26eeb
seekolugopi@gmail.com <seekolugopi@gmail.com>
2024-10-29 11:50:02 +0530

    first commit

---
+++ db/migrate/20241029060751_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[7.2]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :description
+      t.string :checked
+      t.string :boolean
+
+      t.timestamps
+    end
+  end
+end
